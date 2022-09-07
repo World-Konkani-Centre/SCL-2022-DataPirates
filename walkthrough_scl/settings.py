df69f1d3c954e5c9e30c840d687439a21ee1aed2
Rishabh Naik <70832462+RishabhNaik@users.noreply.github.com>
2022-09-08 01:04:42 +0530

    bug fix v6

--- walkthrough_scl/settings.py
+++ walkthrough_scl/settings.py
@@ -16,7 +16,7 @@ import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -157,6 +157,9 @@ MEDIA_URL = '/media/'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 
